b962a83cb260958bc1f2b1b8b4f3931a8b6cec19
Paul Rail <paul.a.rail@gmail.com>
2019-05-18 18:51:06 -0400

    using React Fragments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDom from 'react-dom';
 
 // class App extends Component
@@ -7,6 +7,32 @@ import ReactDom from 'react-dom';
 
 const App = () => {
   return(
+    <Fragment>
+      <nav className="navigation">
+        <a><img src="http://www.stickpng.com/assets/images/584830f5cef1014c0b5e4aa1.png"/></a>
+        <ul>
+          <li>Home</li>
+        </ul>
+      </nav>
+      <main>
+        <div className="leftContent">
+          <img src="http://www.stickpng.com/assets/images/584830f5cef1014c0b5e4aa1.png"/>
+          <form>
+            <h2 className="formTitle">Add a bookmark</h2>
+            <div>
+              <label for="linkTitle" className="formLabel">Enter a name for your bookmark</label>
+            </div>
+            <div>
+
+            </div>
+            <button></button>
+          </form>
+        </div>
+        <div className="rightContent">
+
+        </div>
+      </main>
+    </Fragment>
     <div>
       <h1>Hello World from Hooks!</h1>
     </div>
